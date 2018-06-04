4dd345c81d93de7dd05b702f083c0e456d6a1c98
cobravsninja <cobravsninja@gmail.com>
2018-06-04 22:15:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var util = require('util');
 
 http.createServer(function(request, response) {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.write('fixed version, hostname: ' + os.hostname() + '\n');
+    response.write('0.3 version (fixed), hostname: ' + os.hostname() + '\n');
     response.write('from: ' + request.connection.remoteAddress + '\n');
     response.write('headers:' + '\n');
     response.end(util.inspect(request.headers));
