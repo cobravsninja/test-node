f9c4e68cb402ad5920580e4af86350dc2a9df154
cobravsninja <cobravsninja@gmail.com>
2018-06-04 22:01:51 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var os = require('os');
+var util = require('util');
+
+http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write('hostname: ' + os.hostname() + '\n');
+    response.write('from: ' + request.connection.remoteAddress + '\n');
+    response.write('headers:' + '\n');
+    response.end(util.inspect(request.headers));
+}).listen(6969)
