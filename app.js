277a41f3478143864636b9318abb563444dec1ff
cobravsninja <cobravsninja@gmail.com>
2018-06-04 22:03:10 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var http = require('http');
 var os = require('os');
 var util = require('util');
+var count = 0;    
 
 http.createServer(function(request, response) {
+    if(count >= 3) {                                                                                                                                                          
+        response.writeHead(500, { "Content-Type": "text/plain" });                                                                                                            
+        response.end('fake error');                                                                                                                                           
+        count = 0;                                                                                                                                                            
+        return;                                                                                                                                                               
+    }
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.write('hostname: ' + os.hostname() + '\n');
     response.write('from: ' + request.connection.remoteAddress + '\n');
